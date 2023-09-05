bc9c1d036727c4dc2b706d73f76137f53fc9fe73
mkgupta01 <mk4693480@gmail.com>
2023-09-06 02:54:15 +0530

    App is fully functional

--- src/components/Videocard.jsx
+++ src/components/Videocard.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
-// import thumbnail from '../assets/demo-thumbnail.png'
-// import logo from '../assets/user.jpg'
+import thumbnail from '../assets/default-thumbnail.png'
+import { Link } from 'react-router-dom'
 
-const Videocard = ( props ) => {
+const Videocard = (props) => {
     return (
-        <div className='box-border border-base-red  h-64 w-80 my-4 dark:text-light-white'>
-            <img className='rounded-2xl mb-2' src={ props.thumbnail } alt="thumbnail" />
-            <div id='about-video' className='flex items-center'>
-                <img
-                    id='channelProfile'
-                    className='h-4 w-8 rounded-3xl mr-2 relative bottom-2'
-                    src={ props.thumbnail } alt="profile" />
-                <div>
-                    <p id='title' className='m-1 capitalize text-sm font-semibold tracking-wide'>{ props.title }</p>
+        <Link to={ `/video/${ props.videokey }` }>
+            <div className='h-64 w-80 my-4 dark:text-light-white'>
+                <img className='rounded-2xl mb-2' src={props.data.thumbnails.medium.url || thumbnail} alt="thumbnail" />
+                <div id='about-video' className='flex items-center'>
+                    <img
+                        id='channelProfile'
+                        className='h-10 w-10 rounded-3xl mr-2 relative bottom-2'
+                        src={props.data.thumbnails.medium.url} alt="profile" />
+                    <div>
+                        <p id='title' className='m-1 capitalize text-sm font-semibold tracking-wide'>{props.data.title}</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
